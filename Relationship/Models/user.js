1e9c2b0841afd342ac29631831f551f74cd365de
Sahil Makvana <17bce053@nirmauni.ac.in>
2024-04-20 19:17:52 +0530

    Commit

--- Relationship/Models/user.js
+++ Relationship/Models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 main().catch((err) => {
     console.log(err);
@@ -7,3 +8,38 @@ main().catch((err) => {
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/relationship");
 }
+
+const userSchema = new Schema({
+    username: String,
+    addresses: [
+        {
+            _id: false,
+            location: String,
+            city: String,
+        },
+    ],
+});
+
+const User = mongoose.model("user", userSchema);
+
+const addUser = async () => {
+    let user = new User({
+        username: "Sharelockholems",
+        addresses: [
+            {
+                location: "221B Baker Street",
+                city: "London",
+            },
+        ],
+    });
+
+    user.addresses.push({
+        location: "23, Food Street",
+        city: "New York",
+    });
+
+    let res = await user.save();
+    console.log(res);
+};
+
+addUser();
