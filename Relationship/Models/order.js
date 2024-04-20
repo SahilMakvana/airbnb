3dda890707ff7455aedffea4c406550ee27c2263
Sahil Makvana <17bce053@nirmauni.ac.in>
2024-04-20 19:17:38 +0530

    Commit

---
+++ Relationship/Models/order.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+main().catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/relationship");
+}
+
+const orserSchema = new Schema({
+    item: String,
+    price: Number,
+});
+
+const Order = mongoose.model("order", orserSchema);
+
+const addOrder = async () => {
+    let res = await Order.insertMany(
+        {
+            item: "Pizza",
+            price: 279,
+        },
+        {
+            item: "Samosa",
+            price: 12,
+        },
+        {
+            item: "Chips",
+            price: 40,
+        },
+        {
+            item: "Chocolates",
+            price: 100,
+        }
+    );
+
+    console.log(res);
+};
+
+addOrder();
